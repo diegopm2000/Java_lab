933611453fd0a5b2498b16011ac44198bf16b544
Diego Perez <diegopm2000@gmail.com>
2020-04-24 18:39:22 +0200

    complete

---
+++ 05-files/streamingfiles/src/main/java/com/files/App.java
@@ -0,0 +1,12 @@
+package com.files;
+
+import com.files.NewFile;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+      NewFile fi = new NewFile();
+      fi.write();        
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Since JDK 11
