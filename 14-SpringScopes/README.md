0fcd3f13db11a6ab59d8321f4f459a55732402e6
Diego Perez <diegopm2000@gmail.com>
2020-04-30 13:17:46 +0200

    Spring scopes
    singleton vs prototype

---
+++ 14-SpringScopes/README.md
@@ -0,0 +1,8 @@
+# Spring scopes
+
+Use of scopes in the bean definition
+
+singleton: always return the same instance of the object
+prototype: multiple instances allowed
+
+by default, Spring uses singleton as scope definition of the bean
---
+++ 14-SpringScopes/springscopes/src/main/java/com/springscopes/App.java
@@ -0,0 +1,25 @@
+package com.springscopes;
+
+import com.springscopes.beans.Person;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("com/springscopes/xml/beans.xml");
+        
+        Person myPerson = (Person) appContext.getBean("person");
+        Person myPerson2 = (Person) appContext.getBean("person");
+
+        System.out.println(myPerson);
+        System.out.println(myPerson2);
+
+        System.out.println("id: " + myPerson.getId() + ", name: " + myPerson.getName() + ", surname: " + myPerson.getSurname() + ", country: " + myPerson.getCountry().getCity().getName());
+
+        ((ConfigurableApplicationContext) appContext).close();
+    }
+}
