06ef3551dcdd9b0d3526019ed07fbc41b3e4d60e
Diego Perez Molinero <diegopm2000@gmail.com>
2024-08-11 17:26:46 +0200

    wip: working in progress

---
+++ 05-SpringBoot/SpringBoot-14-SpringBoot3-CRUD-WithService/src/main/java/com/example/demo/api/CarRestController.java
@@ -0,0 +1,59 @@
+package com.example.demo.api;
+
+import com.example.demo.application.CarService;
+import com.example.demo.domain.Car;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CarRestController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarRestController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/cars")
+    public List<Car> getAllCars() {
+
+        return this.carService.getAll();
+    }
+
+    @GetMapping("/cars/{id}")
+    public Car getCarById(@PathVariable Long id) {
+
+        return this.carService.getById(id);
+    }
+
+    @PostMapping("/cars")
+    public ResponseEntity<Car> createCar(@Valid @RequestBody Car newCar) {
+        Car carResult = this.carService.create(newCar);
+        return new ResponseEntity<>(carResult, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/cars/{id}")
+    public ResponseEntity<Car> updateCar(@PathVariable Long id, @RequestBody @Valid CarInputForUpdateDTO updateInputData) {
+        Car carResult = this.carService.update(id, updateInputData);
+        return new ResponseEntity<>(carResult, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/cars/{id}")
+    public ResponseEntity<Void> deleteCar(@PathVariable Long id) {
+        boolean removed = this.carService.delete(id);
+
+        if (removed) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            throw new CarNotFoundException(id);
+        }
+    }
+}
---
+++ 05-SpringBoot/SpringBoot-15-SpringBoot3-CRUD-WithServiceAndRepoInMem/README.md
@@ -0,0 +1,5 @@
+# Spring Boot 3 API REST CRUD with Services
+
+A spring boot 3 API REST CRUD implemented (business logic implemented in services, database in memory))
+
+
